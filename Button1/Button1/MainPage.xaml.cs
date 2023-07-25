4fedde6447780467db7bbdfc310d29200bac03c4
28904 <28904@KNOBBLEBOY>
2023-07-24 23:27:01 -0600

    Agregar archivos de proyecto.

---
+++ Button1/Button1/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Button1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        ///<Summary>
+        ///<Createddate>24/07/2023</Createddate>
+        ///<company>INDEL</company>
+        ///<lastmodificationdate>24/07/2023</lastmodificationdate>
+        ///<lastmodificationdescription>
+        ///Este metodo permite cambiar el texto al boton al presionar
+        ///
+        ///</lastmodificationdescription>
+        //////<lastmodifierautor>Fernando Jose Majano Aguirre</lastmodifierautor>
+        ///</Summary>
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Presióname de nuevo";
+        }
+    }
+}
